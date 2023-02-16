1ca8c0a8e176a296d97d8cd1ff0fd7f35f83ef38
tiemeni <tiemanirocket@gmail.com>
2023-02-16 15:47:01 +0100

    somme changes

--- src/Components/MainComponent/index.js
+++ src/Components/MainComponent/index.js
@@ -13,7 +13,7 @@ export default function MainComponent() {
   const handleClose = () => setOpen(false);
   return (
     <div>
-      <Button onClick={handleOpen}>Ouvrir la modale</Button>
+      <Button onClick={handleOpen}>Start</Button>
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
