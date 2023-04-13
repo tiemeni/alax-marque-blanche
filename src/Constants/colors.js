ac1eff13cfcc69aaa89592e655c272002cb37f15
kouamkouammichellewis <kouamkouammichellewis@gmail.com>
2023-04-13 12:35:33 +0100

    fix: affichage modal principal

--- src/Constants/colors.js
+++ src/Constants/colors.js
@@ -1,5 +1,8 @@
-export const colors = {
-    primaryColor: "#04b7c9",
-    secondary: "#fdb305",
-    inputBorder: '#c4c4c4'
-}
+const colors = {
+    alaxOrange: '#FDB305',
+    vertTurquois: '#04B7C9',
+    gris: '#D9D9D9',
+
+};
+
+export default colors;
